50f976ac77e1818a39abf24c01bb2853164f3182
Roman Barsuchenko <RnBako@mail.ru>
2021-09-01 14:03:01 +0300

    Lesson 5, task 1. totalWeight added

--- 03_ObjectsAndMethods/src/Basket.java
+++ 03_ObjectsAndMethods/src/Basket.java
@@ -4,6 +4,7 @@ public class Basket {
     private String items = "";
     private int totalPrice = 0;
     private int limit;
+    private double totalWeight = 0;
 
     public Basket() {
         increaseCount(1);
@@ -31,10 +32,10 @@ public class Basket {
     }
 
     public void add(String name, int price) {
-        add(name, price, 1);
+        add(name, price, 1, 0);
     }
 
-    public void add(String name, int price, int count) {
+    public void add(String name, int price, int count, double weight) {
         boolean error = false;
         if (contains(name)) {
             error = true;
@@ -52,6 +53,7 @@ public class Basket {
         items = items + "\n" + name + " - " +
             count + " шт. - " + price;
         totalPrice = totalPrice + count * price;
+        totalWeight = totalWeight + count * weight;
     }
 
     public void clear() {
@@ -62,6 +64,8 @@ public class Basket {
     public int getTotalPrice() {
         return totalPrice;
     }
+
+    public  double getTotalWeight() { return totalWeight; }
 
     public boolean contains(String name) {
         return items.contains(name);
