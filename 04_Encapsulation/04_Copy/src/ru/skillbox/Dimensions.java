b092e6728914c60abdac960ea639d6a43f0f5e0f
Roman Barsuchenko <RnBako@mail.ru>
2021-09-03 09:33:09 +0300

    Lesson 4, task 4. Copy classes, classes Dimensions changed

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Dimensions.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Dimensions.java
@@ -4,27 +4,41 @@ public class Dimensions {
     private final int width;
     private final int height;
     private final int length;
+    private final int volume;
 
-    public Dimensions(int width, int height, int lenght) {
+    public Dimensions(int width, int height, int length) {
         this.width = width;
         this.height = height;
-        this.length = lenght;
+        this.length = length;
+        volume = width * height * length;
     }
 
     public int getWidth() {
         return width;
     }
 
+    public Dimensions setWidth(int width) {
+        return new Dimensions(width, height, length);
+    }
+
     public int getHeight() {
         return height;
+    }
+
+    public Dimensions setHeight(int height) {
+        return new Dimensions(width, height, length);
     }
 
     public int getLength() {
         return length;
     }
 
+    public Dimensions setLength(int length) {
+        return new Dimensions(width, height, length);
+    }
+
     //Получение объема груза
     public int getVolume() {
-        return (width * height * length);
+        return volume;
     }
 }
