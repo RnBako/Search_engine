9c157c0a750533b9f31d341f423097256dec047b
Roman Barsuchenko <RnBako@mail.ru>
2021-09-07 10:22:23 +0300

    Lesson 6. Primitives, classes Main and Container changed

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -8,5 +8,16 @@ public class Main {
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
+        for (int i = 0; i < 65536; i++) {
+            if (i == (int)'Ё' || i == (int)'ё') {
+                System.out.println(i + " - " + (char) i);
+            } else  if (i >= (int)'А' && i <= (int)'я' ) {
+                System.out.println(i + " - " + (char) i);
+            }
+        }
+
+//        for (char c = 'Ё'; c <= 'ё'; c++) {
+//            System.out.println(c + " - " + (int) c);
+//        }
     }
 }
