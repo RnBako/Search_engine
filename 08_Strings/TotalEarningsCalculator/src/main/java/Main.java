f9a0321f606d3646c3fd5313fb0a4008ec8f714b
Roman Barsuchenko <RnBako@mail.ru>
2021-09-09 20:31:48 +0300

    Lesson 8, Task 1. Salary calculated

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+    String salaryCharSet = "руб";
+
+    String vasyaSalary = text.substring(text.substring(0, text.indexOf(salaryCharSet)).trim().lastIndexOf(" "), text.indexOf(salaryCharSet)-1);
+    String remainingText = text.substring(text.indexOf(salaryCharSet) + 3);
+
+    String petyaSalary = remainingText.substring(remainingText.substring(0, remainingText.indexOf(salaryCharSet)).trim().lastIndexOf(" "), remainingText.indexOf(salaryCharSet)-1);
+    remainingText = remainingText.substring(remainingText.indexOf(salaryCharSet) + 3);
+
+    String mashaSalary = remainingText.substring(remainingText.substring(0, remainingText.indexOf(salaryCharSet)).trim().lastIndexOf(" "), remainingText.indexOf(salaryCharSet)-1);
+
+    System.out.println("Сумма заработка всех друзей: " + (Integer.parseInt(vasyaSalary.trim()) + Integer.parseInt(petyaSalary.trim()) + Integer.parseInt(mashaSalary.trim())) + " рублей");
   }
 
 }
