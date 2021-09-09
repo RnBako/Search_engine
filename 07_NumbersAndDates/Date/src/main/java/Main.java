4920cc190d8dd4e553db0521d8a09d8796321e9d
Roman Barsuchenko <RnBako@mail.ru>
2021-09-09 09:35:11 +0300

    Lesson 7.10, Task 1. collectBirthdays changed

--- 07_NumbersAndDates/Date/src/main/java/Main.java
+++ 07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
 public class Main {
+
+    public static final DateTimeFormatter FORMATTER_FOR_BIRTHDAY = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+    public static final DateTimeFormatter FORMATTER_FOR_DAY = DateTimeFormatter.ofPattern("EEE").localizedBy(new Locale("us"));
 
     public static void main(String[] args) {
 
@@ -12,11 +19,21 @@ public class Main {
     }
 
     public static String collectBirthdays(int year, int month, int day) {
-
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
-        
-        return "";
+        String birthdayCollection = "";
+        for (int i = 0; i <=today.compareTo(birthday); i++) {
+            if (birthday.plusYears(i).isBefore(today) || birthday.plusYears(i).isEqual(today)) {
+                birthdayCollection = birthdayCollection + i + " - " +
+                                     FORMATTER_FOR_BIRTHDAY.format(birthday.plusYears(i)) + " - " +
+                                     FORMATTER_FOR_DAY.format(birthday.plusYears(i)) +
+                                     System.lineSeparator();
+            }
+        }
+
+        return birthdayCollection;
     }
 }
