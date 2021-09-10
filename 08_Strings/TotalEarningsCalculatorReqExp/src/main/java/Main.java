577925733dd091b43e55a05247d2f88b41a66782
Roman Barsuchenko <RnBako@mail.ru>
2021-09-10 10:12:05 +0300

    Lesson 8, Task 3. Salary regex calculated

--- 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    System.out.println("Сумма заработка всех друзей: " + calculateSalarySum(input));
   }
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    return -1;
+    String regex = "[\\s][0-9]{1,9}[\\sруб]";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    int salarySum = 0;
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      salarySum += Integer.parseInt(text.substring(start, end).trim());
+    }
+    return salarySum;
   }
 
 }
