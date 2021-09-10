54dce61d5978d80ccb580566202aed4edac5ad0b
Roman Barsuchenko <RnBako@mail.ru>
2021-09-10 16:50:41 +0300

    Lesson 8, Task 5. FullNameFormatterRegex added

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -12,6 +14,21 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      String regex1 = "[А-я,-]{1,}[\\s][А-я]{1,}[\\s][А-я]{1,}";
+      Pattern pattern = Pattern.compile(regex1);
+      Matcher matcher = pattern.matcher(input);
+      if (matcher.find() && input.length() == matcher.end()) {
+        String regex2 = "[А-я,-]{1,}";
+        pattern = Pattern.compile(regex2);
+        matcher = pattern.matcher(input);
+        if (matcher.find()) { System.out.println("Фамилия: " + input.substring(matcher.start(), matcher.end())); }
+        if (matcher.find()) { System.out.println("Имя: " + input.substring(matcher.start(), matcher.end())); }
+        if (matcher.find()) { System.out.println("Отчество: " + input.substring(matcher.start(), matcher.end())); }
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+        continue;
+      }
     }
   }
 
