2778dd96c09c7b5521b394063b67d37bd932c085
Roman Barsuchenko <RnBako@mail.ru>
2021-09-10 11:59:41 +0300

    Lesson 8, Task 4. SplitText changed

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    System.out.println(System.lineSeparator() + splitTextIntoWords(input));
   }
 
   public static String splitTextIntoWords(String text) {
     //TODO реализуйте метод
-    return "";
+    String cleanText = text.replaceAll("[^A-z’]"," ");
+
+    String regex = "[A-z’]{1,}[\\s]";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(cleanText);
+    String textResult = "";
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      textResult = textResult + ((textResult.isEmpty()) ? "" : System.lineSeparator()) + cleanText.substring(start, end).trim();
+    }
+    return textResult;
   }
 
 }
