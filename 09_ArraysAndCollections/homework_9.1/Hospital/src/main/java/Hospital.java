df4ad2afe42ed1f7f8a7a132b7367e5b7f433718
Roman Barsuchenko <RnBako@mail.ru>
2021-09-14 16:10:10 +0300

    Lesson 9, Task 1.2. Hospital class created

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -3,8 +3,11 @@ public class Hospital {
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+        float[] temperatureData = new float[patientsCount];
+        for (int i = 0; i < temperatureData.length; i++) {
+            temperatureData[i] =  (float) (Math.ceil(Math.random() * 10) / 10) * (40 - 32) + 32;
+        }
+        return temperatureData;
     }
 
     public static String getReport(float[] temperatureData) {
@@ -12,11 +15,24 @@ public class Hospital {
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
+        String patientsTemperatures = "";
+        float sumTemperature = 0;
+        int healthyCount = 0;
+        for (int i = 0; i < temperatureData.length; i++) {
+            if (i != temperatureData.length-1) {
+                patientsTemperatures = patientsTemperatures + temperatureData[i] + " ";
+            } else {
+                patientsTemperatures = patientsTemperatures + temperatureData[i];
+            }
+            sumTemperature += temperatureData[i];
+            if (temperatureData[i] >= 36.2F && temperatureData[i] <= 36.9F) { healthyCount++; }
+        }
+        float averageTemperature = (float) (int) (sumTemperature / temperatureData.length * 100) / 100;
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + patientsTemperatures +
+                        "\nСредняя температура: " + averageTemperature +
+                        "\nКоличество здоровых: " + healthyCount;
 
         return report;
     }
