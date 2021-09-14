861a5773bc7da66b01258acace28e558a611d6c4
Roman Barsuchenko <RnBako@mail.ru>
2021-09-14 14:34:55 +0300

    Lesson 9, Task 1.1. Reverse array added

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,11 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+        for (int i = 0; i < strings.length / 2; i++) {
+            String tempString = strings[i];
+            strings[i] = strings[strings.length - i - 1];
+            strings[strings.length - i - 1] = tempString;
+        }
         return strings;
     }
 }
