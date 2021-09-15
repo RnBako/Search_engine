60e0250e20bd4f0d472c7b067a30b9312f244b2a
Roman Barsuchenko <RnBako@mail.ru>
2021-09-15 10:27:49 +0300

    Lesson 9, Task 3. Emaillist created + main

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
+    public static final EmailList emailList = new EmailList();
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -21,13 +21,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
+            System.out.print("Введите комманду (доступны LIST, ADD, 0): ");
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
             
             //TODO: write code here
-            
+            if (input.matches("LIST")) {
+                for (String email : emailList.getSortedEmails()) {
+                    System.out.println(email);
+                }
+            } else if (input.matches("ADD .*")) {
+                emailList.add(input.substring(4));
+            } else {
+                System.out.println("Введена не верная команда, попробуйте еще раз.");
+            }
         }
     }
 }
