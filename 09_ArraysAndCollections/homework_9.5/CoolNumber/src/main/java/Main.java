155e8e083d704ce3243e2ff73218be49f02a8c3b
Roman Barsuchenko <RnBako@mail.ru>
2021-09-15 21:09:36 +0300

    Lesson 9, Task 5. CoolNumbers created

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import net.sf.saxon.functions.Trace;
+
+import java.util.*;
+
 public class Main {
     /*
     TODO:
@@ -7,6 +11,40 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        List<String> list = CoolNumbers.generateCoolNumbers();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите номер в формате Х000ХХ000 или 0 для выхода: ");
+            String number = scanner.nextLine();
 
+            if (number.matches("[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{3}")) {
+                long startTime = System.nanoTime();
+                boolean isFind = CoolNumbers.bruteForceSearchInList(list, number);
+                long finishTime = System.nanoTime();
+                System.out.println("Поиск перебором: номер " + (isFind ? "найден" : "не найден") + ", поиск занял " + (finishTime - startTime) + "нс");
+
+                Collections.sort(list);
+                startTime = System.nanoTime();
+                isFind = CoolNumbers.binarySearchInList(list, number);
+                finishTime = System.nanoTime();
+                System.out.println("Бинарный поиск: номер " + (isFind ? "найден" : "не найден") + ", поиск занял " + (finishTime - startTime) + "нс");
+
+                HashSet<String> hashList = new HashSet<>(list);
+                startTime = System.nanoTime();
+                isFind = CoolNumbers.searchInHashSet(hashList, number);
+                finishTime = System.nanoTime();
+                System.out.println("Поиск в HashSet: номер " + (isFind ? "найден" : "не найден") + ", поиск занял " + (finishTime - startTime) + "нс");
+
+                TreeSet<String> treeList = new TreeSet<>(list);
+                startTime = System.nanoTime();
+                isFind = CoolNumbers.searchInTreeSet(treeList, number);
+                finishTime = System.nanoTime();
+                System.out.println("Поиск в TreeSet: номер " + (isFind ? "найден" : "не найден") + ", поиск занял " + (finishTime - startTime) + "нс");
+            } else if (number.equals("0")) {
+                break;
+            } else {
+                System.out.println("Не верный формат номера. Попробуйте еще раз.");
+            }
+        }
     }
 }
