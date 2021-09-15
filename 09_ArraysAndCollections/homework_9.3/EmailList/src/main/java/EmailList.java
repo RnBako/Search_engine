5af554565cd3da63a9adedd6c5b5f85cfce9f282
Roman Barsuchenko <RnBako@mail.ru>
2021-09-15 10:26:32 +0300

    Lesson 9, Task 3. Emaillist created

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,18 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class EmailList {
-
+    private final TreeSet<String> emailList = new TreeSet<>();
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        email = email.toLowerCase(Locale.ROOT);
+        if (email.matches("[A-z0-9]*@[A-z0-9]*\\.[A-z]{2,3}")) {
+            emailList.add(email);
+        } else { System.out.println(Main.WRONG_EMAIL_ANSWER); }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new ArrayList<String>(emailList);
     }
 
 }
