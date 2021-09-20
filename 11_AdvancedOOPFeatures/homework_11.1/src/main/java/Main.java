76632b40d02059063ac6a0bbd6561e46d8a38ef2
Roman Barsuchenko <RnBako@mail.ru>
2021-09-20 10:01:55 +0300

    Lesson 11, Task 1. sortBySalaryAndAlphabet created

--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -1,15 +1,23 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+    public static final String STAFF_TXT = "C:/Users/AVK/Documents/Java/java_basics/11_AdvancedOOPFeatures/homework_11.1/data/staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        sortBySalaryAndAlphabet(staff);
         System.out.println(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff, ((o1, o2) -> {
+            if (o1.getSalary() > o2.getSalary()) { return 1; }
+            else if (o1.getSalary() < o2.getSalary()) { return -1; }
+            else {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }));
     }
 }
