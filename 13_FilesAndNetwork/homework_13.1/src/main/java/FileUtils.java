064b187fa0f40e75047dd5ffcd74569cc1f03fb6
Roman Barsuchenko <RnBako@mail.ru>
2021-09-25 10:36:44 +0300

    Lesson 13, Task 1. class FileUtils added

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long size = 0;
+        try (Stream<Path> walk = Files.walk(Path.of(path))) {
+            size = walk.filter(Files::isRegularFile).mapToLong(p -> {
+                try {
+                    return Files.size(p);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    return 0L;
+                }
+            }).sum();
+        } catch ( IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return size;
     }
 }
--- 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/Main.java
@@ -1,5 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            System.out.println("Введите путь к папке в формате \"C:\\Папка\\:");
+            Scanner scanner = new Scanner(System.in);
+            String path = scanner.nextLine().trim();
+            System.out.println("Размер папки " + path + " составляет " + recalculateSize(FileUtils.calculateFolderSize(path)));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
+    private static String recalculateSize(long size) {
+        double sizeInKBytes = (double) size / (1024);
+        if (sizeInKBytes < 1024) return (String.format("%.2f", sizeInKBytes) + " Кбайт");
+        double sizeInMBytes = (double) sizeInKBytes / 1024;
+        if (sizeInMBytes < 1024) return (String.format("%.2f", sizeInMBytes) + " Мбайт");
+        double sizeInGBytes = (double) sizeInMBytes / 1024;
+        return (String.format("%.2f", sizeInGBytes) + " Гбайт");
     }
 }
