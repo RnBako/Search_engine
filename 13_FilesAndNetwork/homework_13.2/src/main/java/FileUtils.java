b1d66267e708a0578c3900d3c9c61919897c39d9
Roman Barsuchenko <RnBako@mail.ru>
2021-09-27 15:59:44 +0300

    Lesson 13, Task 2. class FileUtils for copy added

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,25 @@
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try (Stream<Path> walk = Files.walk(Path.of(sourceDirectory))) {
+            walk.forEach(p -> {
+                try {
+                    if (!sourceDirectory.equals(p.toString())) {
+                        System.out.println("Скопировали: " + Files.copy(p, Path.of(destinationDirectory + p.toString().substring(sourceDirectory.length())), StandardCopyOption.REPLACE_EXISTING));
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
