b984ddd4ae1b42ddbe1aa22101b21ee008d9fe85
Roman Barsuchenko <RnBako@mail.ru>
2021-10-04 19:25:26 +0300

    Lesson 14, Task 2. Hibernate code changed

--- 14_SQLAndHibernate/jdbc-exp-hibernate/src/main/java/Main.java
+++ 14_SQLAndHibernate/jdbc-exp-hibernate/src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
