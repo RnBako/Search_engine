c227e6964c23768ebbe0c57132e2b97d8a3e6e0f
Roman Barsuchenko <RnBako@mail.ru>
2022-02-23 16:14:04 +0300

    Diploma. Intermediate version. Added logging. Autotests development first step

---
+++ Diplom/SiteSearchEngine/src/main/resources/db.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `field` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `selector` varchar(255) NOT NULL,
+  `weight` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `site` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `last_error` text,
+  `name` varchar(255) NOT NULL,
+  `status` enum('INDEXING','INDEXED','FAILED') NOT NULL,
+  `status_time` datetime NOT NULL,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `lemma` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `frequency` int NOT NULL,
+  `lemma` varchar(255) NOT NULL,
+  `site_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5hcty8fjeaxphmttfkqtoxeyw` (`site_id`),
+  CONSTRAINT `FK5hcty8fjeaxphmttfkqtoxeyw` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=296015 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `page` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `code` int NOT NULL,
+  `content` mediumtext NOT NULL,
+  `path` text NOT NULL,
+  `site_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKm6i94ts2a5h1symo57t4yjdra` (`site_id`),
+  CONSTRAINT `FKm6i94ts2a5h1symo57t4yjdra` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10211 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `index` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rank` float NOT NULL,
+  `lemma_id` int NOT NULL,
+  `page_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKiqgm34dkvjdt7kobg71xlbr33` (`lemma_id`),
+  KEY `FKsq3363uoow6fmurlfheackwgc` (`page_id`),
+  CONSTRAINT `FKiqgm34dkvjdt7kobg71xlbr33` FOREIGN KEY (`lemma_id`) REFERENCES `lemma` (`id`),
+  CONSTRAINT `FKsq3363uoow6fmurlfheackwgc` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=322216 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- Diplom/SiteSearchEngine/src/test/java/main/IndexRepositoryTest.java
+++ Diplom/SiteSearchEngine/src/test/java/main/IndexRepositoryTest.java
@@ -1,26 +1,56 @@
 package main;
 
-import junit.framework.TestCase;
 import model.*;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import repository.IndexRepository;
 import repository.LemmaRepository;
 import repository.PageRepository;
 import repository.SiteRepository;
 
-import java.util.ArrayList;
+import static org.junit.Assert.*;
+
 import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class IndexRepositoryTest extends TestCase {
+@Testcontainers
+@ContextConfiguration(initializers = {IndexRepositoryTest.Initializer.class})
+public class IndexRepositoryTest {
+
+    @ClassRule
+    public static MySQLContainer<?> database = new MySQLContainer<>("mysql:latest")
+            .withDatabaseName("springboot")
+            .withUsername("springboot")
+            .withPassword("springboot");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + database.getJdbcUrl(),
+                    "spring.datasource.username=" + database.getUsername(),
+                    "spring.datasource.password=" + database.getPassword(),
+                    "spring.jpa.hibernate.ddl-auto = update"
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
     @Autowired
     private IndexRepository indexRepository;
@@ -45,10 +75,10 @@ public class IndexRepositoryTest extends TestCase {
         Index index = new Index(page, lemma, (float) 0.9);
         indexRepository.save(index);
 
-        List<Index> indexList = indexRepository.findByLemmaAndLemmaSize("1", 1);
-//        Iterable<Index> indexIterable = indexRepository.findAll();
-//        List<Index> indexList = new ArrayList<>();
-//        indexIterable.forEach(indexList::add);
+        Integer lemmas = lemma.getId();
+
+        List<Index> indexList = indexRepository.findByLemmaAndLemmaSize(lemmas.toString(), 1);
+        System.out.println("Count index - " + indexList.size() + ", and - " + indexRepository.findAll().size());
         assertEquals(index, indexList.get(0));
     }
 }
