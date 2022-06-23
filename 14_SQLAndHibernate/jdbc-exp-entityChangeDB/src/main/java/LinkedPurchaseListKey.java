372f196dcee225d5cc48b4371d305139761e9c40
Roman Barsuchenko <RnBako@mail.ru>
2022-06-23 13:02:10 +0300

    Return assignments by course

---
+++ 14_SQLAndHibernate/jdbc-exp-entityChangeDB/src/main/java/LinkedPurchaseListKey.java
@@ -0,0 +1,51 @@
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class LinkedPurchaseListKey implements Serializable {
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "student_id", nullable = false, insertable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Student student;
+
+
+    @ManyToOne ( fetch = FetchType.EAGER)
+    @JoinColumn(name = "course_id", nullable = false, insertable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Course course;
+
+    public LinkedPurchaseListKey() {
+        super();
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LinkedPurchaseListKey that = (LinkedPurchaseListKey) o;
+        return student.equals(that.student) && course.equals(that.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(student, course);
+    }
+}
